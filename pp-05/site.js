1af2d46447dba51dc3bd79234d33b695f47448be
anny Lin <ylin95@hawk.iit.edu>
2017-11-13 22:04:58 -0600

    adding button

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,16 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+$(document).ready(function(){
+
+  $("#doorbell").remove();
+
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+
+  $('#ring').on('click', function() {
+    ringDoorbell();
+    console.log("DoorBell Ring!!!");
+  }
+
+
+});
